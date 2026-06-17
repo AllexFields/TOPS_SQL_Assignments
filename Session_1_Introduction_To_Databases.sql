9fdf2dbf90a86d6a13adbc5b55a3265c51a4cb04
Shivam Thakur <allex3xmas@gmail.com>
2026-06-17 15:12:58 +0530

    Add files via upload

---
+++ Session_1_Introduction_To_Databases.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE food_delivery_db;
+
+------- Three Differences between MySQL & PostgreSQL --------
+
+--  Feature / Use Case       | MySQL                                                                      | PostgreSQL                                                                                                          |
+--  ------------------------ | -------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------- |
+--    1. Performance Focus   | Optimized for fast read-heavy web applications and simple queries.         | Optimized for complex queries, analytics, and large-scale data processing.                                          |
+--    2. Advanced Features   | Simpler feature set, easier for beginners to learn and manage.             | Supports advanced features such as JSONB, window functions, Common Table Expressions (CTEs), and custom data types. |
+--    3. Typical Use Cases   | Commonly used for content management systems, blogs, and web applications. | Commonly used for enterprise applications, financial systems, GIS, and data analytics platforms.                    |
+
+
+-- Popular Users of MySQL are Facebook, WordPress...
+-- Popular Users of PostgreSQL are Instagram, Reddit...
---
+++ Session_2_Basic_Select_From.sql
@@ -0,0 +1,52 @@
+USE session_8;
+
+----- 1. Select all columns -----
+
+CREATE TABLE restaurants (
+    restaurant_id INT PRIMARY KEY,
+    restaurant_name VARCHAR(100),
+    location VARCHAR(100),
+    rating DECIMAL(2,1)
+);
+
+INSERT INTO restaurants
+VALUES
+(1, 'Spice Garden', 'Ahmedabad', 4.5);
+
+
+
+SELECT * FROM restaurants;
+
+----- 2. Display only name & rating columns -----
+
+CREATE TABLE zomato_reviews (
+    review_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    rating DECIMAL(2,1),
+    review_text VARCHAR(255)
+);
+
+SELECT name,rating FROM zomato_reviews;
+
+----- 3. Rename column names -----
+
+CREATE TABLE movies (
+    movie_id INT PRIMARY KEY,
+    movie_name VARCHAR(100),
+    release_year INT,
+    genre VARCHAR(50)
+);
+
+SELECT movie_name AS "Title", release_year AS "Year Released" FROM movies;
+
+----- 4. Add Comments using '-' -------
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+-- This query retrieves all columns and all rows from the products table.
+SELECT * FROM products;
